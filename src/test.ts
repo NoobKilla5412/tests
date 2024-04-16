e2b0e73a607c05419d6a23320a3464af9ae8cf5f
NoobKilla5412 <brandoncarlrobertson@gmail.com>
2024-04-16 11:44:52 -0700

    Update

--- src/test.ts
+++ src/test.ts
@@ -8,7 +8,7 @@ function calculateProgressBar(percent: number, cap = 100) {
   let res = "";
 
   for (let i = 0; i < cap; i++) {
-    if (i == index) res += "X";
+    if (i == index) res += "|";
     else res += ".";
   }
 
@@ -17,8 +17,8 @@ function calculateProgressBar(percent: number, cap = 100) {
 
 export async function test() {
   const res = document.body.appendChild(document.createElement("div"));
-  for (let i = 0; i <= 1000; i++) {
-    res.innerHTML = `${calculateProgressBar(i / 1000)} - ${i / 10}%`;
-    await sleep(10);
+  for (let i = 0; i <= 100; i++) {
+    res.innerHTML = `${calculateProgressBar(i / 100)} - ${i}%`;
+    await sleep(100);
   }
 }
